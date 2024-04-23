bd8729bfcd5ec3eeeeab411e52eb4278ed815b9a
Adel <adel.sh@gmail.com>
2024-04-23 11:13:29 +0200

    changing prompt for ollama synthetic data generation

--- datasets/synthetic data/ollama_test.py
+++ datasets/synthetic data/ollama_test.py
@@ -2,7 +2,7 @@ import ollama
 response = ollama.chat(model='llama3:8b', messages=[
   {
     'role': 'user',
-    "content": f"List the top 10 common typos or spelling mistakes of the word computer. Don't o\
+    "content": f"List the top 10 common typos or spelling mistakes of the input word computer. Don't o\
 ... utput anything else other than the comma separated list of common typos. Don't provide any other context. Don't include the input word in the list. Don't include synynoms of the input words as misspelled words. Don't output any notes or remarks",
   },
 ])
