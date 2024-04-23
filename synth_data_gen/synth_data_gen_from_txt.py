d53c2fa5186db133f26e265cf94bf1651bf4244b
Adel <adel.sh@gmail.com>
2024-04-23 16:49:03 +0200

    change from chat to generate mode in ollama

--- synth_data_gen/synth_data_gen_from_txt.py
+++ synth_data_gen/synth_data_gen_from_txt.py
@@ -3,12 +3,9 @@ import ollama
 
 def show_common_typos_ollama(word):
     # Call to the Ollama API using the llama3 model
-    response = ollama.chat(model='llama3', messages=[
-        {
-            'role': 'user',
-            "content": f"List only five common non-word spelling errors for the word '{word}'. These errors should be common typos that an English native speaker would make regularly. The errrors shoulo not form real English words. The typos should be more than 1 characters long each. Only provide a comma-separated list of these errors. Ensure the list does never include the word '{word}' or synonyms of the word '{word}'. Ensure that the answer never include any additinal text. The list of errors should be ranked from most common to the least commmon"
-        },
-    ])
+    response = ollama.generate(
+        model='llama3', 
+        prompt=f"List only ten common non-word spelling errors for the word '{word}'. These errors should be common typos that an English native speaker would make regularly. The errrors shoulo not form real English words. The typos should be more than 1 characters long each. Only provide a comma-separated list of these errors. Ensure the list does never include the word '{word}' or synonyms of the word '{word}'. Ensure that the answer never include any additinal text. The list of errors should be ranked from most common to the least commmon",)
     return response
 
 def save_typos_to_jsonl(source_file_path, output_file_path):
@@ -27,7 +24,7 @@ def save_typos_to_jsonl(source_file_path, output_file_path):
             
 
             # Assuming the typo_response returns JSON with a specific structure. Adjust according to actual API response.
-            typos_list = typo_response['message']['content']
+            typos_list = typo_response['response']
             print(words_count,word,"->",typos_list)
 
             # Create a JSON object for each line and write to the output file
