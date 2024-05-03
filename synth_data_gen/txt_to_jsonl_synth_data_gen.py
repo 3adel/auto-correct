69cd5cabc6d3195785ce938ca55e5b55d0650cb7
Adel <adel.sh@gmail.com>
2024-05-03 16:30:08 +0200

    uploading files

--- synth_data_gen/txt_to_jsonl_synth_data_gen.py
+++ synth_data_gen/txt_to_jsonl_synth_data_gen.py
@@ -4,7 +4,7 @@ import ollama
 def show_common_typos_ollama(word):
     # Call to the Ollama API using the llama3 model
     response = ollama.generate(
-        model='llama3', 
+        model='llama3:8b', 
         prompt=f"List only ten common non-word spelling errors for the word '{word}'. These errors should be common typos that an English native speaker would make regularly. The errrors shoulo not form real English words. The typos should be more than 1 characters long each. Only provide a comma-separated list of these errors. Ensure the list does never include the word '{word}' or synonyms of the word '{word}'. Ensure that the answer never include any additinal text. The list of errors should be ranked from most common to the least commmon",
         options={
             "temperature": 0.1,
@@ -45,11 +45,15 @@ output_file_path_10_words = "/Users/adel/adel/dev/training_data/auto-correct/gwo
 output_file_path_100_words = "/Users/adel/adel/dev/training_data/auto-correct/gwords/jsonified/frequency-alpha-alldicts_100_words.jsonl"
 output_file_path_1000_words = "/Users/adel/adel/dev/training_data/auto-correct/gwords/jsonified/frequency-alpha-alldicts_1000_words.jsonl"
 output_file_path_2000_words = "/Users/adel/adel/dev/training_data/auto-correct/gwords/jsonified/frequency-alpha-alldicts_2000_words.jsonl"
+output_file_path_4000_words = "/Users/adel/adel/dev/training_data/auto-correct/gwords/jsonified/frequency-alpha-alldicts_4000_words.jsonl"
 output_file_path_10000_words = "/Users/adel/adel/dev/training_data/auto-correct/gwords/jsonified/frequency-alpha-alldicts_10000_words.jsonl"
 
 # Call the function to process and save typos
 # save_typos_to_jsonl(source_file_path, output_file_path_10_words,10)
 # save_typos_to_jsonl(source_file_path, output_file_path_100_words,100)
-save_typos_to_jsonl(source_file_path, output_file_path_1000_words,1000)
+# save_typos_to_jsonl(source_file_path, output_file_path_1000_words,1000)
 #save_typos_to_jsonl(source_file_path, output_file_path_2000_words,2000)
+#save_typos_to_jsonl(source_file_path, output_file_path_4000_words,4000)
+save_typos_to_jsonl(source_file_path, output_file_path_10000_words,10000)
+
 # save_typos_to_jsonl(source_file_path, output_file_path_10000_words,10000)
